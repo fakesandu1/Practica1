7cd99f50d9f47d2faf1f0e6d922a2374affd890c
vali <fakesandu1@gmmail.com>
2026-07-14 12:18:14 +0300

    start of GUI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from pandas.core.interchange import column
+
 import database as db
 import numpy as np
+import customtkinter
 
 
 class error_detection:
@@ -9,6 +12,7 @@ class error_detection:
     minus_tols=[]
     dev=[]
     outtol=[]
+    maindata=[]
     def __init__(self):
         for i in range(len(db.data.sensory)):
             self.noms.append(db.data.sensory[i][3])
@@ -23,16 +27,42 @@ class error_detection:
         self.meas = np.array(self.meas)
         self.dev = np.array(self.dev)
         self.outtol=np.array(self.outtol)
+        self.maindata=np.array(self.maindata)
     def deviations(self):
         outside=np.where(self.outtol != 0)
         outside=np.append(outside,outside)
+        maindata=[]
         print(outside)
         for i in outside:
-            print(db.data.sensory[i])
+            #print(db.data.sensory[i])
+            maindata.append(db.data.sensory[i])
+        maindata=np.array(maindata)
+        self.maindata=np.append(self.maindata,maindata)
+        print(self.maindata)
 
 
 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1080×768")
 
 
-eroare=error_detection()
-eroare.deviations()
+        self.textbox = customtkinter.CTkTextbox(master=self,width=1080,height=768,corner_radius=0)
+        self.textbox.grid(row=0,column=0,sticky="nsew")
+        self.textbox.insert("0.0","test")
+
+        self.button=customtkinter.CTkButton(self,text="test",command=self.button_callback)
+        self.button.grid(row=0,column=0,padx=5,pady=5)
+
+    def button_callback(self):
+        eroare = error_detection()
+        eroare.deviations()
+
+        for i in range(0,len(eroare.maindata)):
+            self.textbox.insert("0.0","%s\n"%eroare.maindata)
+
+if __name__=="__main__":
+
+    app=App()
+    app.mainloop()
